01f007b52586e19a742ca45f7f9cf09899480796
thenoobchad <noobchaddy@gmail.com>
2025-06-29 21:25:47 +0100

    login function with cookie

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { createError } from "../utils/error.js";
 import { connectToDB } from "../utils/connect.js";
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
 
 
 export async function register(req, res, next) {
@@ -26,7 +27,27 @@ export async function register(req, res, next) {
 }
 
 export async function login(req, res, next) {
-    res.send("new login route")
+    const data = req.body;
+    console.log(data);
+    if (!data?.email || !data?.password) {
+        return next(createError(400, "Missing fields."))
+    }
+
+    await connectToDB();
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return next(createError(400, "Invalid credentials"));
+const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+
+
+    if (!isPasswordCorrect) return next(createError(400, "Invalid credentials."))
+    
+    const token = jwt.sign({ id: user._id }, process.env.JWT);
+    console.log(token)
+    res.cookie("access_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production"
+    }).status(200).json("User logged in.")
 }
 
 export async function logout(req, res, next) {
